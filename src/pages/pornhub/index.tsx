78eb30e99c18851431ed9388e501c1ab9487f3ca
Mashiirox <Mashiirox@foxmail.com>
2022-04-27 17:12:07 +0800

    feat: Add export in pornhub-page

--- src/pages/pornhub/index.tsx
+++ src/pages/pornhub/index.tsx
@@ -1,28 +1,47 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import html2canvas from 'html2canvas';
 
 import Setting from './setting';
 
 import './index.scss';
 
+const initialSetting = {
+  fontSize: 72,
+  hubBackgroundColor: '#ff9900',
+  pornTextColor: '#ffffff',
+  hubTextColor: '#000000',
+};
+
 function PornHub(): JSX.Element {
-  const [setting, setSetting] = useState({
-    fontSize: 72,
-    hubBackgroundColor: '#ff9900',
-    pornTextColor: '#ffffff',
-    hubTextColor: '#000000',
-  });
+  const [setting, setSetting] = useState(initialSetting);
+  const previewRef = useRef<HTMLDivElement>(null);
 
   const { fontSize, hubBackgroundColor, pornTextColor, hubTextColor } = setting;
+
+  const handleExport = (): void => {
+    html2canvas(previewRef.current!, {
+      allowTaint: false,
+      useCORS: true,
+      scrollY: 0,
+    }).then((canvas) => {
+      const dataImg = new Image();
+      dataImg.src = canvas.toDataURL('img/png');
+      const alink = document.createElement('a');
+      alink.href = dataImg.src;
+      alink.download = 'logo.png';
+      alink.click();
+    });
+  };
 
   return (
     <div className='ph-page'>
       <div className='w-full pt-10 flex justify-center mb-24'>
-        <span className='ph-logo bg-black text-white'>Make</span>
-        <span className='ph-logo ph-logo-logo text-black'>Logo</span>
+        <span className='ph-logo bg-black text-white'>Porn</span>
+        <span className='ph-logo ph-logo-logo'>Hub</span>
       </div>
       <div className='ph-preview'>
         <div className='ph-preview-box'>
-          <div className='ph-view' id='preview'>
+          <div className='ph-view' ref={previewRef}>
             <span
               contentEditable
               suppressContentEditableWarning
@@ -45,6 +64,11 @@ function PornHub(): JSX.Element {
         </div>
       </div>
       <Setting onChange={(value): void => setSetting(value)} />
+      <div className='flex justify-center mt-20'>
+        <div className='ph-export' onClick={handleExport}>
+          export
+        </div>
+      </div>
     </div>
   );
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path'; // 设置了 allowSyntheticDefaultImports 仍有报错提示，故如此导入 =_+
+
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: {
+      '@': _resolve('src'),
+      '@a/*': _resolve('src/assets'),
+      '@c/*': _resolve('src/components'),
+    },
+  },
+  plugins: [react()],
+});
