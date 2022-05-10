0d36c591ceb5b5a01c07ed7e37a023a3b94a8acf
Mashiirox <Mashiirox@foxmail.com>
2022-05-10 16:39:44 +0800

    fix: Remove minesweeper

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,7 +5,6 @@ import PageLoading from '@c/page-loading';
 import Home from '@/pages/home';
 
 const PornHub = lazy(() => import('@/pages/pornhub'));
-const Minesweeper = lazy(() => import('@/pages/minesweeper'));
 
 function Routes(): JSX.Element {
   return (
@@ -13,7 +12,6 @@ function Routes(): JSX.Element {
       <RouterRoutes>
         <Route path='/' element={<Home />} />
         <Route path='/pornhub' element={<PornHub />} />
-        <Route path='/minesweeper' element={<Minesweeper />} />
       </RouterRoutes>
     </Suspense>
   );
