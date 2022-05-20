18b6ba609cd2691c0646d4a8a6554f7e80fb81c2
Mashiirox <Mashiirox@foxmail.com>
2022-05-20 16:55:51 +0800

    feat: Gitalk

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom';
+import GitalkComponent from 'gitalk/dist/gitalk-component';
+
 import Icon from '@c/icon';
+
+import 'gitalk/dist/gitalk.css';
 
 function Home(): JSX.Element {
   return (
     <div className='bg-red'>
       <Link to='./pornhub'>Hello World</Link>
       <Icon name='camera' color='red' />
+      <GitalkComponent
+        options={{
+          clientID: '7134ff1c7af78c3f7bdf',
+          clientSecret: 'd2c54280daadca07020d03754b6a6d7aa7f744c2',
+          repo: 'gitalk_comment',
+          owner: 'Mashiirox',
+          admin: ['Mashiirox'],
+          id: 'mashiirox.com', // Ensure uniqueness and length less than 50
+          distractionFreeMode: false, // Facebook-like distraction free mode
+        }}
+      />
     </div>
   );
 }
---
+++ src/pages/home/type.d.ts
@@ -0,0 +1,1 @@
+declare module 'gitalk/dist/gitalk-component';
